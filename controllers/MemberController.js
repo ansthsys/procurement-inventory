06eb24c07700e6ee2b7743593f20f9d705f1b1ff
ansthsys <azmiythufail91@gmail.com>
2023-05-19 15:03:54 +0700

    feat: create method show in member controller

--- controllers/MemberController.js
+++ controllers/MemberController.js
@@ -3,12 +3,33 @@ const { User } = require("../models");
 class MemberController {
   static async index(req, res) {
     const data = await User.findAll({
-      attributes: { exclude: ["password"] },
+      attributes: { exclude: ["password", "createdAt", "updatedAt"] },
     });
 
     res.status(200).json({
       success: true,
       message: "get all members",
+      data,
+    });
+  }
+
+  static async show(req, res) {
+    const id = req.params.id;
+    const data = await User.findOne({
+      where: { id: id },
+      attributes: { exclude: ["password"] },
+    });
+
+    if (!data) {
+      return res.status(404).json({
+        success: false,
+        message: "member not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "get member by id",
       data,
     });
   }
