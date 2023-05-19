eda2a75704fe8eaa7e0265930c8c37df2d160d92
ansthsys <azmiythufail91@gmail.com>
2023-05-19 14:27:17 +0700

    feat: create routing for authentication

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const multer = require("multer");
 const app = express();
 const forms = multer();
 
+const authRouter = require("./routes/auth");
+
 app.use(forms.array());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(authRouter);
 
 module.exports = app;
