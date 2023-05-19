2ef5feac59b0ac7571de2674b26b274ab1f99216
ansthsys <azmiythufail91@gmail.com>
2023-05-19 16:38:21 +0700

    feat: create method insert in member controller

--- controllers/MemberController.js
+++ controllers/MemberController.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+const { Op } = require("sequelize");
 const { User } = require("../models");
 
 class MemberController {
@@ -30,6 +32,54 @@ class MemberController {
     return res.status(200).json({
       success: true,
       message: "get member by id",
+      data,
+    });
+  }
+
+  static async insert(req, res) {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "request body is requred",
+      });
+    }
+
+    const lastData = await User.findOne({
+      where: { username: { [Op.substring]: "MBR" } },
+      attributes: ["username"],
+    });
+
+    const lastUsername = lastData.username.split("-")[1];
+    const newNumber = `${parseInt(lastUsername) + 1}`.padStart(4, "0");
+    const newUsername = `MBR-${newNumber}`;
+
+    const emailExists = await User.findOne({
+      where: { email },
+    });
+
+    if (emailExists) {
+      return res.status(400).json({
+        success: false,
+        message: "email already used",
+      });
+    }
+
+    const data = await User.create({
+      name,
+      username: newUsername,
+      email,
+      password: bcrypt.hashSync(password, 8),
+      role: "member",
+      status: "active",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "new member created",
       data,
     });
   }
