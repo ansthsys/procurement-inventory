9a5c9d92ecd4bebc9356532a1cc76df58c805498
ansthsys <azmiythufail91@gmail.com>
2023-05-19 14:16:34 +0700

    feat: create users and items migrations

---
+++ migrations/20230518093922-create-item.js
@@ -0,0 +1,66 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Items", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      category: {
+        allowNull: false,
+        type: Sequelize.ENUM("object", "vechile"),
+      },
+      qty: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DECIMAL,
+      },
+      total_price: {
+        type: Sequelize.DECIMAL,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      url_request: {
+        type: Sequelize.TEXT,
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.ENUM("process", "approve", "reject"),
+      },
+      pickup_date: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Items");
+  },
+};
