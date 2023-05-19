0d67d6050cebd7e2570c127b7b5c96be4b4ab312
ansthsys <azmiythufail91@gmail.com>
2023-05-19 14:42:21 +0700

    fix: change route and controller name admin

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,12 +2,41 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../../middlewares/auth");
 const adminMiddleware = require("../../middlewares/admin");
-const UserController = require("../../controllers/UserController");
+const MemberController = require("../../controllers/MemberController");
 
-router.get("/admin/", authMiddleware, adminMiddleware, UserController.index);
-router.get("/admin/:id", UserController.show);
-router.post("/admin/", UserController.insert);
-router.put("/admin/:id", UserController.update);
-router.delete("/admin/:id", UserController.destroy);
+/**
+ * 
+ *  Member management by admin
+ */
+router.get(
+  "/admin/member/",
+  authMiddleware,
+  adminMiddleware,
+  MemberController.index
+);
+router.get(
+  "/admin/member/:id",
+  authMiddleware,
+  adminMiddleware,
+  MemberController.show
+);
+router.post(
+  "/admin/member/",
+  authMiddleware,
+  adminMiddleware,
+  MemberController.insert
+);
+router.put(
+  "/admin/member/:id",
+  authMiddleware,
+  adminMiddleware,
+  MemberController.update
+);
+router.delete(
+  "/admin/member/:id",
+  authMiddleware,
+  adminMiddleware,
+  MemberController.destroy
+);
 
 module.exports = router;
