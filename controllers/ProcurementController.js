fcbec5efc1ec92b9dcd7893209a4f8ab08fd5278
ansthsys <azmiythufail91@gmail.com>
2023-05-22 19:09:43 +0700

    feat: add method index in procurement controller

--- controllers/ProcurementController.js
+++ controllers/ProcurementController.js
@@ -1,5 +1,19 @@
 const { User, Item } = require("../models");
 
-class ProcurementController {}
+class ProcurementController {
+  static async index(req, res) {
+    const data = await Item.findAll({
+      where: {
+        status: "process"
+      }
+    })
+
+    return res.status(200).json({
+      success: true,
+      message: "get all data procurement",
+      data,
+    });
+  }
+}
 
 module.exports = ProcurementController;
