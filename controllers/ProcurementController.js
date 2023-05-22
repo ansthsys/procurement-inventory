32be28c98552fa7763141d43321c210cf080ca2f
ansthsys <azmiythufail91@gmail.com>
2023-05-22 20:29:17 +0700

    feat: joining the table for data response

--- controllers/ProcurementController.js
+++ controllers/ProcurementController.js
@@ -3,10 +3,13 @@ const { User, Item } = require("../models");
 class ProcurementController {
   static async index(req, res) {
     const data = await Item.findAll({
-      where: {
-        status: "process"
-      }
-    })
+      where: { status: "process" },
+      include: {
+        model: User,
+        as: "user",
+        attributes: { exclude: ["password"] },
+      },
+    });
 
     return res.status(200).json({
       success: true,
