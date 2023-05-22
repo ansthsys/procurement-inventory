0f506b0681641abbf066120f989f61a69c09055f
ansthsys <azmiythufail91@gmail.com>
2023-05-22 21:34:35 +0700

    feat: add method process in procurement controller

--- controllers/ProcurementController.js
+++ controllers/ProcurementController.js
@@ -69,6 +69,69 @@ class ProcurementController {
       data,
     });
   }
+
+  static async process(req, res) {
+    const id = req.params.id;
+    const { status } = req.body;
+    const allowedProcess = ["approve", "reject"];
+
+    if (!id || isNaN(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "id is required",
+      });
+    }
+
+    if (!status) {
+      return res.status(400).json({
+        success: false,
+        message: "field body is required",
+      });
+    }
+
+    if (allowedProcess.indexOf(status) === -1) {
+      return res.status(400).json({
+        success: false,
+        message: "invalid process status",
+      });
+    }
+
+    const checkData = await Item.findByPk(id);
+
+    if (!checkData) {
+      return res.status(404).json({
+        success: false,
+        message: "item not found",
+      });
+    }
+
+    if (checkData.status !== "process") {
+      return res.status(400).json({
+        success: false,
+        message: "item already processed",
+      });
+    }
+
+    const data = await Item.update(
+      {
+        status: status,
+        updatedAt: new Date(),
+      },
+      { where: { id } }
+    );
+
+    if (!data[0]) {
+      return res.status(400).json({
+        success: false,
+        message: "failed to process item",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "update member successfully",
+    });
+  }
 }
 
 module.exports = ProcurementController;
