66f6194a2ddce84389bbecaa14a04502b127dfbd
ansthsys <azmiythufail91@gmail.com>
2023-05-23 21:31:38 +0700

    feat: create method index in dashboard controller

---
+++ controllers/DashboardMemberController.js
@@ -0,0 +1,30 @@
+const { Item } = require("../models");
+
+class DashboardAdminController {
+  static async index(req, res) {
+    const { id: userId } = req.user;
+    const total = await Item.count({ where: { userId } });
+    const processed = await Item.count({
+      where: { userId, status: "process" },
+    });
+    const approved = await Item.count({
+      where: { userId, status: "approve" },
+    });
+    const rejected = await Item.count({
+      where: { userId, status: "reject" },
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "get data for dashboard",
+      requestInventory: {
+        total,
+        processed: processed,
+        approved: approved,
+        rejected: rejected,
+      },
+    });
+  }
+}
+
+module.exports = DashboardAdminController;
