3ff5803d135d9788db1c61b0cdc14da1a6ca8506
ansthsys <azmiythufail91@gmail.com>
2023-05-24 15:21:04 +0700

    feat: create method show in procurement member

--- controllers/Member/ProcurementController.js
+++ controllers/Member/ProcurementController.js
@@ -28,6 +28,41 @@ class ProcurementController {
       data,
     });
   }
+
+  static async show(req, res) {
+    const id = req.params.id;
+    const { id: userId } = req.user;
+
+    if (!id || isNaN(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "id is required",
+      });
+    }
+
+    const data = await Item.findOne({
+      where: { id, userId },
+      attributes: { exclude: ["UserId"] },
+      include: {
+        model: User,
+        as: "user",
+        attributes: { exclude: ["password"] },
+      },
+    });
+
+    if (!data) {
+      return res.status(404).json({
+        success: false,
+        message: "item not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "get item member by id",
+      data,
+    });
+  }
 }
 
 module.exports = ProcurementController;
