037b7bdd1722b964298b0e20d833954e426e4933
ansthsys <azmiythufail91@gmail.com>
2023-05-24 14:50:38 +0700

    feat: add route procurement for member

--- routes/member/index.js
+++ routes/member/index.js
@@ -2,7 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../../middlewares/auth");
 const DashboardController = require("../../controllers/Member/DashboardController");
+const ProcurementContontroller = require("../../controllers/Member/ProcurementController");
 
 router.get("/", authMiddleware, DashboardController.index);
+
+/**
+ *  Dashboard for member
+ */
+router.get("/procurement", authMiddleware, ProcurementContontroller.index);
+router.get("/procurement/:id", authMiddleware, ProcurementContontroller.show);
+router.post("/procurement", authMiddleware, ProcurementContontroller.insert);
 
 module.exports = router;
