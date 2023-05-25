59f5fea2164437e4eadecfe396b3836e4c85d94e
ansthsys <azmiythufail91@gmail.com>
2023-05-25 23:45:53 +0700

    feat: add endpoint for logout account

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const AuthController = require("../../controllers/AuthController");
 
 router.post("/login", AuthController.login);
+router.post("/logout", AuthController.logout);
 
 module.exports = router;
