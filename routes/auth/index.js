8222c6593fee182b6967ee3a45acb806b6a91d87
ansthsys <azmiythufail91@gmail.com>
2023-05-25 23:54:06 +0700

    fix: add midddleware auth to endpoint logout

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../../middlewares/auth");
 const AuthController = require("../../controllers/AuthController");
 
 router.post("/login", AuthController.login);
-router.post("/logout", AuthController.logout);
+router.post("/logout", authMiddleware, AuthController.logout);
 
 module.exports = router;
